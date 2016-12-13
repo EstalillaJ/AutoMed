2b258826d67557527250e935b1a19b9df1fe7bf9
Joshua Estalilla <joshtestalilla@gmail.com>
2016-12-13 12:08:41 -0800

    Add Empty data models

---
+++ AutoMed/Models/Location.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoMed.Models.DataModels
+{
+    public class Location
+    {
+        public string Name { get; set; }
+    }
+}
