3fcb43138e4dc35f1030f07b3b5ebb82205757bb
Joshua Estalilla <joshtestalilla@gmail.com>
2016-12-15 22:26:19 -0800

    Finish Principal and Location Repos, Rename User Repo

---
+++ AutoMed/DAL/LocationRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using AutoMed.Models;
+
+namespace AutoMed.DAL
+{
+    public class LocationRepository : ILocationRepository
+    {
+        public LocationRepository()
+        {
+
+        }
+
+        public void AddLocation(Location location)
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {   
+                context.Locations.Add(location);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteLocation(Location location)
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                context.Locations.Remove(context.Locations.Find(location.Id));
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateLocation(Location location)
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                context.Entry(location).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Location> SelectAll()
+        {
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                return context.Locations.ToList();
+            }
+        }
+    }
+}
---
+++ AutoMed/DAL/PrincipalRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMed.Models;
+using AutoMed.Models.DataModels;
+using System.Data.Entity;
+
+namespace AutoMed.DAL
+{
+    public class PrincipalRepository : IPrincipalRepository
+    {   
+        public PrincipalRepository()
+        {
+
+        }
+
+        public void Create(AutoMedPrincipal principal, string password)
+        {
+            using (ApplicationContext Context = new ApplicationContext())
+            {
+                if (Context.AutoMedUsers.Any(x => x.Name == principal.Name))
+                    throw new Exception("That Username Is Taken");
+                AutoMedUser user = new AutoMedUser
+                {
+                    Name = principal.Identity.Name,
+                    Role = principal.Role.ToString(),
+                    Password = password,
+                    Location = principal.Location
+                };
+                Context.AutoMedUsers.Add(user);
+                Context.Entry(user.Location).State = EntityState.Unchanged;
+                Context.SaveChanges();
+            }
+        }
+
+        public AutoMedPrincipal SelectByUsernameAndPassword(string username, string password)
+        {
+            using (ApplicationContext Context = new ApplicationContext())
+            {
+                AutoMedUser user = 
+                    Context.
+                    AutoMedUsers.
+                    Where(x => x.Password == password && x.Name == username).
+                    Include("Location").FirstOrDefault();
+
+                if (user == null)
+                    return null;
+
+                AutoMedPrincipal principal = new AutoMedPrincipal
+                {
+                    Location = user.Location,
+                    Id = user.Id,
+                    Name = user.Name,
+                    Role = (AutoMedPrincipal.Roles)Enum.Parse(typeof(AutoMedPrincipal.Roles), user.Role)
+                };
+                return principal;
+            }
+        }
+
+        public bool TryEditUserPassword(AutoMedPrincipal principal, string oldPassword, string newPassword)
+        {
+            using (ApplicationContext Context = new ApplicationContext())
+            {
+                AutoMedUser user = Context.AutoMedUsers.Where(x => x.Password == oldPassword && x.Name == principal.Name).FirstOrDefault();
+                if (user == null)
+                    return false;
+                user.Password = newPassword;
+                Context.Entry(user).Property(x => x.Password).IsModified = true;
+                Context.SaveChanges();
+                return true;
+            }
+        }
+
+        public void Update(AutoMedPrincipal principal)
+        {
+            using (ApplicationContext Context = new ApplicationContext())
+            {
+                AutoMedUser user = new AutoMedUser()
+                    {
+                        Role = principal.Role.ToString(),
+                        Id = principal.Id,
+                        LocationId = principal.Location.Id
+                    };
+
+                Context.Entry(user).State = EntityState.Modified;
+                Context.Entry(user).Property(x => x.Password).IsModified = false;
+                Context.Entry(user).Property(x => x.Name).IsModified = false;
+
+                Context.SaveChanges();
+            }
+        }
+
+        public void Delete(AutoMedPrincipal principal)
+        {
+            using (ApplicationContext Context = new ApplicationContext())
+            {
+                AutoMedUser user = new AutoMedUser { Id = principal.Id };
+                Context.AutoMedUsers.Remove(Context.AutoMedUsers.Find(principal.Id));
+                Context.SaveChanges();
+            }
+        }
+        
+         
+    }
+}
