4b9c54ef39331fa86a8409d90cfbc1880858139f
Joshua Estalilla <joshtestalilla@gmail.com>
2016-12-16 14:28:27 -0800

    this shouldve been in last commit

--- AutoMed/DAL/ApplicationContext.cs
+++ AutoMed/DAL/ApplicationContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using AutoMed.Models.DataModels;
 using AutoMed.Models;
 
@@ -10,20 +6,16 @@ namespace AutoMed.DAL
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<AutoMedUser> AutoMedUsers { get; set; }
+        public DbSet<AutoMedUserDataModel> AutoMedUsers { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Document> Documents { get; set; }
-        public DbSet<Quote> Quotes { get; set; }
-	public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<QuoteDataModel> QuoteDataModels { get; set; }
+	    public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Location> Locations { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-                
-
             base.OnModelCreating(modelBuilder);
         }
-
     }
-
 }
