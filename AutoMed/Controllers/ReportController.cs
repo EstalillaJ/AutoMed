8da7339d90cb9cc04f2322a8824ea29c0d70b390
Joshua Estalilla <joshtestalilla@gmail.com>
2016-12-23 20:54:38 -0800

    First pass at Report creation page

---
+++ AutoMed/Controllers/ReportController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using AutoMed.Models;
+using AutoMed.DAL;
+using System.Text;
+
+namespace AutoMed.Controllers
+{   
+    //[Authorize(Roles="Administrator")]
+    public class ReportController : Controller
+    {
+        private ApplicationDbContext Context = ApplicationDbContext.Create();
+        public ActionResult Create()
+        {
+            CreateReportViewModel viewModel = new CreateReportViewModel()
+            {
+                Locations = new SelectList(Context.Locations, "Id", "Name"),
+                MinDiscountDollars = 0,
+                MaxDiscountDollars = 100
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public FileResult Create(CreateReportViewModel model)
+        {
+            string file = "file, another file\n name, another name";
+
+            return File(Encoding.ASCII.GetBytes(file), "text/plain", string.Format("Report_{0}.csv", DateTime.Now));
+        }
+    }
+}
---
+++ AutoMed/Models/ReportViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace AutoMed.Models
+{
+    public class CreateReportViewModel
+    {   
+        [Display(Name="Include All Locations")]
+        public bool AllLocations { get; set; }
+        [Display(Name="Include All Discount Amounts")]
+        public bool AnyDiscountAmount { get; set; }
+        [Display(Name="Include All Dates")]
+        public bool AnyDate { get; set; }
+        public SelectList Locations { get; set; }
+        public int SelectedLocationId { get; set; }
+        [Display(Name = "Max Discount ($)")]
+        public double MaxDiscountDollars { get; set; }
+        [Display(Name = "Min Discount ($)")]
+        public double MinDiscountDollars { get; set; }
+        [Display(Name = "Year")]
+        public int StartYear { get; set; }
+        [Display(Name = "Month")]
+        public int StartMonth { get; set; }
+        [Display(Name = "Day")]
+        public int StartDay { get; set; }
+        [Display(Name = "Year")]
+        public int EndYear { get; set; }
+        [Display(Name = "Month")]
+        public int EndMonth { get; set; }
+        [Display(Name = "Day")]
+        public int EndDay { get; set; }
+    }
+}
