9c354cc0929792005981f72415f725a6f4c7d96f
Vance Jones <vancejns85@gmail.com>
2017-01-12 15:43:20 -0800

    Some simple changes

---
+++ AutoMed/Controllers/CustomerDetailsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoMed.Controllers
+{
+    public class Vehicles : Controller
+    {
+        // GET: CustomerDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+    
+}
---
+++ AutoMed/Models/CustomerDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoMed.Models
+{
+    public class CustomerDetails
+    {
+        public Vehicle Vehicle { get; set; }
+        public Quote Quote { get; set; }
+    }
+}
