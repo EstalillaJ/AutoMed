759508cc292f0238598c6baa5aafa3fd883c724e
Christian <christiankaseburg@gmail.com>
2017-01-19 11:28:52 -0800

    Deleted QuoteReview controller and views and created QuoteControler with the view in there. Fixed populating the table with only pending quo
    Need to add location in to count when updating the quote approval table.
    Need to finish the button and that is it.

---
+++ AutoMed/Controllers/QuoteController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoMed.DAL;
+using AutoMed.Models;
+
+namespace AutoMed.Controllers
+{
+    public class QuoteController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Quote
+        public ActionResult Index()
+        {
+            return View(db.Quotes.ToList().Where(x => x.Approval.Equals(QuoteStatus.Pending)));
+        }
+
+        // GET: Quote/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Quote/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Quote/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Quote/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Quote/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Quote/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Quote/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
