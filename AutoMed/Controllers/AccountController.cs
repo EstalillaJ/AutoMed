3d1742f93acff58c5758c06b3ed80a7d7d1298cf
jasonBurke <jasonburke135@gmail.com>
2017-01-29 19:10:41 -0800

    take away login when registering

--- AutoMed/Controllers/AccountController.cs
+++ AutoMed/Controllers/AccountController.cs
@@ -215,8 +215,6 @@ namespace AutoMed.Controllers
                 }
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
