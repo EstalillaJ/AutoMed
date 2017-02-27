38906a0e59af800cf6b8350e73386639d6e55028
Joshua Estalilla <joshtestalilla@gmail.com>
2017-02-27 02:16:41 -0800

    fix css on quote create

--- AutoMed/Scripts/UploadImages.js
+++ AutoMed/Scripts/UploadImages.js
@@ -16,16 +16,16 @@
             return function (e) {
                 // Render thumbnail.
                 var div = document.createElement("div");
-                div.className = "images";
-                div.innerHTML = ["<div class=\"imageContainer\">" +'<img class="thumb" src="', e.target.result,
+                div.className = "images col-md-12";
+                div.innerHTML = ["<div class=\"imageContainer col-md-offset-4 col-md-8\">" +'<img class="thumb col-md-12" src="', e.target.result,
                     '" title="', escape(theFile.name), '"/>' +
-                    "<br/><span class=\"remove\">Remove Image</span>" + "</div>"].join("");
+                    "<br/><span class=\"remove col-md-12\">Remove Image</span>" + "</div>"].join("");
 
                 document.getElementById("list").insertBefore(div, null);
                 
                 $(".remove").click(function () {
                     
-                    $(this).parent(".imageContainer").remove();
+                    $(this).parent(".imageContainer").parent().remove();
                     document.getElementById("files").value = "";
                     
                 });
